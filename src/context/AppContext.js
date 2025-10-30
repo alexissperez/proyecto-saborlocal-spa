cecb193f87c21ccb20a561b9d7b8682763c07a5d
alexissperez <al.perezv@duocuc.cl>
2025-10-30 10:38:25 -0300

    Detallitos

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,25 +1,30 @@
-import React, { createContext, useState } from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 
-export const AppContext = createContext();
-
-export const AppProvider = ({ children }) => {
-  // El estado carrito es un array de productos
-  const [carrito, setCarrito] = useState([]);
-
-  // Función para agregar productos
-  const agregarAlCarrito = (producto) => {
-    setCarrito([...carrito, producto]);
-  };
-
-  // Nueva función para vaciar el carrito
-  const vaciarCarrito = () => {
-    setCarrito([]);
-  };
+const Cart = () => {
+  const { carrito, vaciarCarrito } = useContext(AppContext);
 
   return (
-    <AppContext.Provider value={{ carrito, agregarAlCarrito, vaciarCarrito }}>
-      {children}
-    </AppContext.Provider>
+    <div>
+      <h3>Mi Carrito</h3>
+      <hr />
+      <h4>Productos en el carrito</h4>
+      <ul>
+        {carrito.map((producto, i) => (
+          <li key={i}>
+            {producto.nombre} - ${producto.precio}
+          </li>
+        ))}
+      </ul>
+      <strong>Total a pagar: ${carrito.reduce((acc, item) => acc + item.precio, 0)}</strong>
+      <br /><br />
+
+      {/* Botón abajo centrado */}
+      <button style={{marginTop: "16px"}} onClick={vaciarCarrito}>Vaciar carrito</button>
+
+      {/* Aquí abajo podrías poner el botón Cerrar */}
+    </div>
   );
 };
 
+export default Cart;
